466a25892b310d6f0e83dcf65eb8638f53e906ce
胡露 <hulu@shein.com>
2023-03-02 15:17:41 +0800

    demo

--- .husky/commit.js
+++ .husky/commit.js
@@ -17,6 +17,8 @@ BRANCH_NAME=$(git symbolic-ref --short HEAD)
 BRANCH_EXCLUDED=$(printf "%s\\\\n" "\${BRANCHES_TO_SKIP[@]}" | grep -c "^$BRANCH_NAME$")
 BRANCH_IN_COMMIT=$(grep -c "\\\\[$BRANCH_NAME\\\\]" $1)
 
+echo "$BRANCH_NAME"
+
 if [ -n "$BRANCH_NAME" ] && ! [[ $BRANCH_EXCLUDED -eq 1 ]] && ! [[ $BRANCH_IN_COMMIT -ge 1 ]]; then
   sed -i .bak -e "1s/^/[$BRANCH_NAME] /" $COMMIT_MSG_FILE
 fi
