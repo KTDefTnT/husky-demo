7b6f4a701d2fc61fc7cf5ea5450015cc992050e3
胡露 <hulu@shein.com>
2023-03-02 15:20:06 +0800

    测试git hook

--- .husky/commit.js
+++ .husky/commit.js
@@ -17,7 +17,7 @@ BRANCH_NAME=$(git symbolic-ref --short HEAD)
 BRANCH_EXCLUDED=$(printf "%s\\\\n" "\${BRANCHES_TO_SKIP[@]}" | grep -c "^$BRANCH_NAME$")
 BRANCH_IN_COMMIT=$(grep -c "\\\\[$BRANCH_NAME\\\\]" $1)
 
-echo "$BRANCH_NAME"
+echo "分支名称 $BRANCH_NAME"
 
 if [ -n "$BRANCH_NAME" ] && ! [[ $BRANCH_EXCLUDED -eq 1 ]] && ! [[ $BRANCH_IN_COMMIT -ge 1 ]]; then
   sed -i .bak -e "1s/^/[$BRANCH_NAME] /" $COMMIT_MSG_FILE
