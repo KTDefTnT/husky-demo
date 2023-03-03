c99a2e9ea29719f7cd4a3ef0e9155aa14572216c
胡露 <hulu@shein.com>
2023-03-03 16:15:10 +0800

    [main] pre-commit lint-staged

--- src/test.js
+++ src/test.js
@@ -4,5 +4,4 @@ const path = require("path");
 const filePath = path.join(__dirname, "test.txt");
 console.log(filePath);
 
-
 fs.writeFileSync(filePath, new Uint8Array(Buffer.from("Hello Node.js")));
