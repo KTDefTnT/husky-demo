f4062699003e941bcbfa3134043d0e2a7648ce00
胡露 <hulu@shein.com>
2023-03-03 14:11:06 +0800

    [main] test

--- src/test.js
+++ src/test.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
-console.log(path.join(__dirname, "test.txt"));
 const filePath = path.join(__dirname, "test.txt");
+console.log(filePath);
+
+
 fs.writeFileSync(filePath, new Uint8Array(Buffer.from("Hello Node.js")));
